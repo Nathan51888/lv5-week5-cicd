ec002489055caceb5bdd0f781b9ce3d71d83d824
Nathan51888 <NathanLiang99@outlook.com>
2024-03-12 05:29:31 +1300

    test commit

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import fibonacci from "./Fibonacci";
 
 dotenv.config();
 
@@ -10,4 +11,9 @@ server.use(express.json());
 
 server.listen(port, () => {
   console.log(`listening to port ${port}`);
+});
+
+server.get("/fibonacci/:upto", (req, res) => {
+  const upto = req.params.upto;
+  res.send(`Fibonacci for ${upto} is ${fibonacci(parseInt(upto))}`);
 });
